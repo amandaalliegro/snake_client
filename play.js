66b5c0367c65c9910be363b07dc9a179538498ca
amandaalliegro <amanda.m.arnaut@gmail.com>
2020-09-23 19:08:15 +0000

    Add function setupInput to play.js, with the following stdin configuration code

--- play.js
+++ play.js
@@ -4,6 +4,26 @@ const connect = require('./client')
  * Establishes connection with the game server
  */
 
+ /**
+ * Setup User Interface 
+ * Specifically, so that we can handle user input via stdin
+ */
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+
+  stdin.on('data', (key) => {
+    // \u0003 maps to ctrl+c input
+  if (key === '\u0003') {
+    process.exit();
+  }
+    process.stdout.write('.');
+  });
+  return stdin;
+}
+
 
 console.log('Connecting ...');
 connect();
